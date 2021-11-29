f8fc7276aa732ac71c2425c97dd3b5c074c92f3a
leslieyi <leslieyi411@gmail.com>
2021-11-29 10:36:57 -0500

    comments

--- bfs.js
+++ bfs.js
@@ -1,86 +1,87 @@
 class Node {
-    constructor(value){
-        this.value = value;
-        this.left = null;
-        this.right = null;
-    }
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
 }
 
 class BinarySearchTree {
-    constructor(){
-        this.root = null;
+  constructor() {
+    this.root = null;
+  }
+  insert(value) {
+    let newNode = new Node(value);
+    if (this.root === null) {
+      this.root = newNode;
+      return this;
     }
-    insert(value){
-        let newNode = new Node(value);
-        if(this.root === null){
-            this.root = newNode;
-            return this;
+    let current = this.root;
+    while (true) {
+      if (value === current.value) return undefined;
+      if (value < current.value) {
+        if (current.left === null) {
+          current.left = newNode;
+          return this;
         }
-        let current = this.root;
-        while(true){
-            if(value === current.value) return undefined;
-            if(value < current.value){
-                if(current.left === null){
-                    current.left = newNode;
-                    return this;
-                }
-                current = current.left;
-            } else {
-                if(current.right === null){
-                    current.right = newNode;
-                    return this;
-                } 
-                current = current.right;
-            }
+        current = current.left;
+      } else {
+        if (current.right === null) {
+          current.right = newNode;
+          return this;
         }
+        current = current.right;
+      }
     }
-    find(value){
-        if(this.root === null) return false;
-        let current = this.root,
-            found = false;
-        while(current && !found){
-            if(value < current.value){
-                current = current.left;
-            } else if(value > current.value){
-                current = current.right;
-            } else {
-                found = true;
-            }
-        }
-        if(!found) return undefined;
-        return current;
+  }
+  find(value) {
+    if (this.root === null) return false;
+    let current = this.root,
+      found = false;
+    while (current && !found) {
+      if (value < current.value) {
+        current = current.left;
+      } else if (value > current.value) {
+        current = current.right;
+      } else {
+        found = true;
+      }
     }
-    contains(value){
-        if(this.root === null) return false;
-        let current = this.root,
-            found = false;
-        while(current && !found){
-            if(value < current.value){
-                current = current.left;
-            } else if(value > current.value){
-                current = current.right;
-            } else {
-                return true;
-            }
-        }
-        return false;
+    if (!found) return undefined;
+    return current;
+  }
+  contains(value) {
+    if (this.root === null) return false;
+    let current = this.root,
+      found = false;
+    while (current && !found) {
+      if (value < current.value) {
+        current = current.left;
+      } else if (value > current.value) {
+        current = current.right;
+      } else {
+        return true;
+      }
     }
-    BFS(){
-        let node = this.root,
-            data = [],
-            queue = [];
-        queue.push(node);
+    return false;
+  }
+  BFS() {
+      //q = fifo
+    let node = this.root,
+      data = [],
+      queue = []; //take things from beginning, and add to the end
+    queue.push(node);
 
-        while(queue.length){
-           node = queue.shift();
-           data.push(node.value);
-           if(node.left) queue.push(node.left);
-           if(node.right) queue.push(node.right);
-        }
-        return data;
+    while (queue.length) {
+      //empty array is truthy in js so we do .length
+      node = queue.shift();//take from the beginning from queue
+      data.push(node.value); //adding into our list //just to make things to see, node.value
+      if (node.left) queue.push(node.left);
+      if (node.right) queue.push(node.right);
     }
+    return data;
+  }
 }
-
 
 let tree = new BinarySearchTree();
 tree.insert(10);
@@ -90,4 +91,3 @@ tree.insert(3);
 tree.insert(8);
 tree.insert(20);
 tree.BFS();
-    
